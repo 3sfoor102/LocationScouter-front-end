d8e4e112e81482bc03b4b39553425b617576e35e
3sfoor102 <3sfoor102@gmail.com>
2026-08-10 11:07:30 +0300

    Removed the PDF to be transfered to the location Details

--- src/pages/LocationForm.jsx
+++ src/pages/LocationForm.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import React, { useRef } from "react";
-import html2pdf from "html2pdf.js";
 
 import * as locationService from "../services/locations";
 import js from "@eslint/js";
@@ -16,22 +15,6 @@ const LocationForm = (props) => {
     imageURL: "",
   };
   const [formData, setFormData] = useState(initialState);
-  const [showReceipt, setShowReceipt] = useState(false);
-  const printAreaRef = useRef();
-
-  const PDFgenerate = (e) => {
-        e.preventDefault(); 
-
-    const element = printAreaRef.current;
-    const options = {
-      margin: 0.5,
-      filename: "A",
-      image: { type: "jpeg", quality: 0.98 },
-      html2canvas: { scale: 2, useCORS: true },
-      jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
-    };
-    html2pdf().set(options).from(element).save()
-  };
 
   const handleChange = (evt) => {
     setFormData({ ...formData, [evt.target.name]: evt.target.value });
@@ -39,7 +22,6 @@ const LocationForm = (props) => {
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    setShowReceipt(true)
     if (locationId) {
       props.handleUpdateLocation(locationId, formData);
     } else {
@@ -88,17 +70,8 @@ const LocationForm = (props) => {
           value={formData.imageURL}
           onChange={handleChange}
         ></input>
-        <button type="button" onClick={PDFgenerate}>Download as PDF</button>
         <button type="submit">SUBMIT</button>
-        <div ref={printAreaRef} className="">
-          <h1>Submission Receipt</h1>
-          <p><strong>Date: </strong>{new Date().toLocaleDateString()}</p>
-          <p><strong>Title: </strong> {formData.title}</p>
-          <p><strong>Description: </strong> {formData.description}</p>
-          {/* <p><stong>Title: </stong> {formData.imageURL}</p> */}
-
-        </div>
-        
+ 
       </form>
     </main>
   );
