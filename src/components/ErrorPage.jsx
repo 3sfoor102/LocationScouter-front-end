ddaa7c3c3375392c78d07b6f67a05355c6c33165
3sfoor102 <3sfoor102@gmail.com>
2026-08-12 15:47:30 +0300

    CSS update in the error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router";
 const ErrorPage = () => {
   return (
-    <>
-      <h3>Error 404!</h3>;
+    <div id="error-title">
+
+      <h3>Error 404!</h3>
       <Link to="/">
         <button className="btn-secondary-action">Go Back Home</button>
       </Link>
-    </>
+    </div>
   );
 };
 
