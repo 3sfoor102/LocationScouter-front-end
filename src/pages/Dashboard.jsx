6698082f7afef5a28e923c9124a888efad641c05
3sfoor102 <3sfoor102@gmail.com>
2026-08-12 20:35:37 +0300

    Update Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,31 +2,15 @@ import { useEffect, useState } from "react";
 import { index } from "../services/user";
 
 const Dashboard = (props) => {
-  const [allUsers, setAllUsers] = useState([]);
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const usersData = await index();
-      setAllUsers(usersData);
-    };
-    fetchUsers();
-  }, []);
-
   return (
     <section>
       <header style={{ marginBottom: "32px", textAlign: "center" }}>
         <h1>Welcome {props.user.username}!</h1>
-        <p style={{ color: "var(--color-text-light)" }}>View All the Users</p>
+        <p style={{ color: "var(--color-text-light)" }}>
+          Scout a Beautiful Location!
+        </p>
       </header>
-      <div className="hoot-list">
-        {allUsers.map((user) => (
-          <div className="card" key={user._id}>
-            <header>
-              <h2 style={{ color: "var(--color-primary)" }}>{user.username}</h2>
-            </header>
-          </div>
-        ))}
-      </div>
+      <div className="hoot-list"></div>
     </section>
   );
 };
